c59b1f3064c05cb01c07ef33fec1cb2955ba81e1
adebello <ade.bello@patientpop.com>
2018-12-26 21:07:32 -0800

    add list, update and delete api endpoints

---
+++ notes-app-api/requests/delete.js
@@ -0,0 +1,20 @@
+import * as dynamoDBLib from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+export async function main (event, context, callback) {
+  const params = {
+    TableName: 'notes',
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    }
+  }
+
+  try {
+    await dynamoDBLib.call('delete', params);
+    return success({ status: true });
+  } catch (e) {
+    console.error(e);
+    return failure({ status: false });
+  }
+}
---
+++ notes-app-api/requests/update.js
@@ -0,0 +1,27 @@
+import * as dynamoDBLib from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+export async function main (event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: 'notes',
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    },
+    UpdateExpression: 'SET content = :content, attachment = : attachment',
+    ExpressionAttributeValues: {
+      ':attachment': data.attachment || null,
+      ':content': data.content || null
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+
+  try {
+    await dynamoDBLib.call('update', params);
+    return success({ status: true });
+  } catch (e) {
+    console.error(e);
+    return failure({ status: false });
+  }
+}
