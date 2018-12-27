89dab9427894a154b128323b7b9330be619aca99
adebello <ade.bello@patientpop.com>
2018-12-26 23:12:11 -0800

    add 404 page

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './containers/home/Home';
+import NotFound from './containers/404/NotFound';
 
 export default () =>
   <Switch>
     <Route path='/' exact component={Home} />
+    {/*<Route path='/other' exact component={NotFound} />*/}
+    { /*Catch all unmatched routes*/ }
+    <Route path='*' exact compontent={NotFound} />
   </Switch>
